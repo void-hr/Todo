1e0cc201f52f9b401ae1f6cfc802f6954c5e8b45
unknown <gsharmaji93@gmail.com>
2022-08-17 02:28:21 +0530

    reactjsproject

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import TodoList from './components/TodoList';
+
+function App() {
+  return (
+    <div className="todo-app">
+      <h1>TODO APP</h1>
+      <TodoList/>
+    </div>
+  );
+}
+
+export default App;
